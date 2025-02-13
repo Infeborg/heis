ffa7087f5f51965676f9ca7cea6f62085af2755a
JohannesBrenn <johabren@stud.ntnu.no>
2025-02-13 11:28:24 +0100

    edit 2

--- queue.h
+++ queue.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <stdlib.h>
+
+
 typedef struct {
     int size;
     char floor; 
     bool up; // invert for down 
-} queue;
+} queue_element;
+
+queue_element queue[];
 
 void enqueue(); // appends to array
 void dequeue(); // pops from array
 void clear_queue();
-queue get_queue(); // gets element and dequeues 
+void get_queue(); // gets element and dequeues 
 char get_first();
+void check_floors();
