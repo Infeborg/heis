b9efffe60c8c4e61c67ae2ce60e3e2c3733f5d3c
Sanntidslab <sanntidslab@noreply.com>
2025-03-13 08:33:08 +0100

    msg2

--- queue.c
+++ queue.c
@@ -23,12 +23,6 @@ void update_queue() {
 
 // True om noe skal gjøres, false om ikke
 bool find_order() {
-<<<<<<< HEAD
-=======
-
-    // Oppdaterer up & down
-    check_cab_floor();
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
    
     // check order
     for (int button = 0; button < N_BUTTONS; button++) { // N_BUTTONS -1 
@@ -53,27 +47,13 @@ void execute_order() {
                 int destination_floor = floor;
                 int dir = fix_dir(button, floor, current_floor, destination_floor);
                 elevio_motorDirection(dir);
-<<<<<<< HEAD
                 // printf("%d", destination_floor);
                 
-=======
-
-                /*
-                int safety_counter = 0;
-                while (elevio_floorSensor() != destination_floor && safety_counter < 1000000) {
-                    safety_counter++;
-                }
-                if (safety_counter >= 1000000) {
-                    printf("ERROR: Infinite loop detected in execute_order!\n");
-                }
-                */
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
                 while (elevio_floorSensor() != destination_floor) { // is moving
                     if (dir > 0) {
                         int stop_floor = find_order_over(current_floor, destination_floor);
                         if (elevio_floorSensor() == stop_floor) { // stops at floor
                             elevio_motorDirection(DIRN_STOP);
-<<<<<<< HEAD
                             //set_state(door_open);
                             queue[button][stop_floor] = false;
                             elevio_buttonLamp(stop_floor, button, 0);
@@ -81,17 +61,12 @@ void execute_order() {
                             open_door();
                             wait_for_close();
                             close_door();
-=======
-                            set_state(door_open);
-                            queue[button][stop_floor] = false;
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
                         }
                     }
                     if(dir < 0) {
                         int stop_floor = find_order_under(current_floor, destination_floor);
                         if (elevio_floorSensor() == stop_floor) { // stops at floor
                             elevio_motorDirection(DIRN_STOP);
-<<<<<<< HEAD
                             //set_state(door_open);
                             queue[button][stop_floor] = false;
                             elevio_buttonLamp(stop_floor, button, 0);
@@ -99,35 +74,10 @@ void execute_order() {
                             wait_for_close();
                             close_door();
                             //set_state(door_open);
-=======
-                            set_state(door_open);
-                            queue[button][stop_floor] = false;
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
                         }
                     }
                 }
             }
-<<<<<<< HEAD
-=======
-        }
-    }    
-}
-
-// Henter verdier fra cabraden
-void check_cab_floor() {
-    for (int floor = 0; floor < N_FLOORS; floor++) {
-        if (queue[2][floor]) {
-            int current_floor = elevio_floorSensor();
-            int dir = current_floor - floor;
-            if (dir < 0) {
-                queue[0][floor] = true;
-                queue[2][floor] = false;
-            }
-            else {
-                queue[1][floor] = true;
-                queue[2][floor] = false;
-            }
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
         }
     }    
 }
@@ -168,17 +118,7 @@ int find_order_over(int current_floor, int destination_floor) {
             }
         }
     }
-<<<<<<< HEAD
     return destination_floor;
-=======
-    for (int i = current_floor - 1; i >= destination_floor && i >= 0; i--) {
-        printf("Checking queue[1][%d] = %d\n", i, queue[1][i]); // Debugging
-        if (queue[1][i]) { 
-            return i; 
-        }
-    }
-    return destination_floor; // If no intermediate stops, return final destination
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
 }
 
 int find_order_under(int current_floor, int destination_floor) {
@@ -192,17 +132,7 @@ int find_order_under(int current_floor, int destination_floor) {
             }
         }
     }
-<<<<<<< HEAD
     return destination_floor;
-=======
-    for (int i = current_floor - 1; i >= destination_floor && i >= 0; i--) {
-        printf("Checking queue[1][%d] = %d\n", i, queue[1][i]); // Debugging
-        if (queue[1][i]) { 
-            return i; 
-        }
-    }
-    return destination_floor; // If no intermediate stops, return final destination
->>>>>>> bbb985bda0373464071a8d6126ddcb019eb2ee5c
 }
 
 
